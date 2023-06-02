c0c828eeff7817444b81700cbcaaac5aab5e52f6
nguyen.nguyen3 <nguyen.nguyen3@hebela.net>
2023-06-02 22:53:38 +0700

    add code week 2

--- Week_2/main.py
+++ Week_2/main.py
@@ -1,13 +1,25 @@
-import findspark
-findspark.init()
-
 from pyspark.sql import SparkSession
-from pyspark.conf import SparkConf
 
-spark=SparkSession.builder\
-    .master("local[*]")\
-    .appName("WordCount")\
-    .getOrCreate()
 
-sc=spark.sparkContext
-textFile = sc.textFile("hdfs://192.168.1.47:9000/data/create_cerrtificate.txt")
+def create_spark_session(URL):
+    spark = SparkSession.builder.master(URL).appName("Week 2")
+    return spark.getOrCreate()
+
+
+if __name__ == "__main__":
+    SPARK_MASTER_URL = "spark://spark:7077"
+    spark = create_spark_session(SPARK_MASTER_URL)
+    text_file = spark.read.text("hdfs://192.168.0.102:9000/data/demo.txt").rdd.map(lambda x: x[0])
+
+    # Tách các từ trong tập tin văn bản
+    words = text_file.flatMap(lambda line: line.split(" "))
+
+    # Đếm số lần xuất hiện của các từ
+    word_counts = words.countByValue()
+
+    # In kết quả
+    for word, count in word_counts.items():
+        print("{}: {}".format(word, count))
+
+    # Dừng Spark context
+    spark.stop()
